ff06f99706c8dbf21de77b3f4ec8cb5b46e1dd78
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2022-09-02 11:29:15 +0200

    [Docker]: Fix docker-compose in order to have pdo_mysql

--- www/index.php
+++ www/index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo 'Hello World!'
+echo 'Hello World! <br />';
+
+$username = 'user';
+$password = 'username';
+
+if (isset($_GET['username']))
+    $username = $_GET['username'];
+
+$bdd = new PDO('mysql:host=db;port=3306;dbname=strouppl;charset=utf8', 'root', 'root');
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$req = $bdd->prepare("SELECT * FROM users");//WHERE username = 'samuel' AND password = 'password123'");
+$req->execute();
+
+while($data = $req->fetch()){
+    echo $data['username'] . ' - ' . $data['password'] . '<br />';
+}
+
+//echo phpinfo();
 
 ?>

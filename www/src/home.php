8198594666309165d9e8b50eb089b0d1045506e9
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2022-09-02 14:24:52 +0200

    [UI]: Add: Home page w/ connect

---
+++ www/src/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Stroupl.io" />
+    <meta name="keywords" content="Stroupl.io" />
+    <meta name="author" content="Nathan Chevalier & Alan Gueret - Stroople Lab" />
+    <meta name="theme-color" content="#09025f" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Stroupl.io</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+    <link rel="icon" type="/image/png" href="/img/favicon.png" />
+    <link rel="stylesheet" href="/src/css/style.css" />
+</head>
+<body class="">
+    <header>
+        <img src="/src/img/logo.png" alt="" />
+    </header>
+
+    <section id="connect">
+        <form method="post" action="index.php">
+            <label for="email" value="Votre adresse mail" >Votre adresse mail</label>
+            <input type="text" name="email" />
+            <label for="password" value="" >Votre mot de passe</label>
+            <input type="password" name="password" />
+
+            <input type="submit" value="Se connecter" class="button"/>
+        </form>
+    </section>
+
+</body>
+</html>

6c76721e9fb8329bd97f31d5507cd0e4e07a3b40
Alan GUERET <agueret@stroople.com>
2022-09-02 16:33:42 +0200

    [index.php] - prepare SQL command

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # strouppl.io
 Fake website for cybersecurity penetration demonstration
+
+# Authors
+
+Nathan CHEVALIER
+Alan GUERET
--- www/index.php
+++ www/index.php
@@ -2,20 +2,22 @@
 
 echo 'Hello World! <br />';
 
-$username = 'user';
-$password = 'username';
+$username = "' OR true; --";
+$password = 'admin';
 
-if (isset($_GET['username']))
-    $username = $_GET['username'];
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
 
 $bdd = new PDO('mysql:host=db;port=3306;dbname=strouppl;charset=utf8', 'root', 'root');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$req = $bdd->prepare("SELECT * FROM users");//WHERE username = 'samuel' AND password = 'password123'");
+$req = $bdd->prepare("SELECT * FROM users WHERE username = '" . $username . "' AND password = '" . $password . "'");
 $req->execute();
 
 while($data = $req->fetch()){
-    echo $data['username'] . ' - ' . $data['password'] . '<br />';
+    echo $data['username'] . ' - ' . $data['password'] . ' : ' . $data['email'] . '<br />';
 }
 
 //echo phpinfo();
